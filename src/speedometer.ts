e9c5c9a6586eb05deefd767929e7f3f5a3a79a9e
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-08-26 16:30:35 +0800

    Try to update position every second

--- src/speedometer.ts
+++ src/speedometer.ts
@@ -5,18 +5,22 @@ export function setupSpeedometer(element: HTMLDivElement) {
     element.innerHTML = `${speed ?? "--"} km/h`;
   };
 
-  navigator.geolocation.watchPosition(
-    (position) => {
-      const { speed: newSpeed } = position.coords;
-      if (newSpeed === null) {
-        setSpeed(null);
-        return;
-      }
+  const updateSpeed = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { speed: newSpeed } = position.coords;
+        if (newSpeed === null) {
+          setSpeed(null);
+          return;
+        }
 
-      const speedInKMH = newSpeed * 3.6;
-      setSpeed(Math.round(speedInKMH * 100) / 100);
-    },
-    null,
-    { enableHighAccuracy: true }
-  );
+        const speedInKMH = newSpeed * 3.6;
+        setSpeed(Math.round(speedInKMH * 100) / 100);
+      },
+      null,
+      { enableHighAccuracy: true }
+    );
+  };
+
+  setInterval(updateSpeed, 1000);
 }
