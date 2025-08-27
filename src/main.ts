962cb330fff6cbe161f43f25ac71161f1e95a28e
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-08-27 12:37:25 +0800

    render basic before js load

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
 import "./style.css";
 import { setupSpeedometer } from "./speedometer.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h2>Speedometer</h2>
-    <h3>Current Speed</h3>
-    <h1 id="speed">-- km/h</h1>
-  </div>
-`;
-
 setupSpeedometer(document.querySelector<HTMLDivElement>("#speed")!);
